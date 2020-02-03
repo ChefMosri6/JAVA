9144979436bb517b0215a67bb972f51ed07baadf
Christian Alejandro Mosri <39068565+ChefMosri6@users.noreply.github.com>
2020-02-02 18:32:05 -0600

    Add files via upload

---
+++ agendaPersonal/src/com/agenda/informacionUsuario.java
@@ -0,0 +1,14 @@
+package com.agenda;
+
+import java.util.Scanner;
+
+public class informacionUsuario{
+    public String nombre;
+    public String apellidoPaterno;
+    public String apellidoMaterno;
+    public String direccion;
+    public String telefono;
+    public int i;
+    static Scanner leer = new Scanner(System.in);
+}
+
